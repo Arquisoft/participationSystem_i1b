fa7450c908e6ee0a2dc2597e7348819ccb2ee597
Tronerfull DEL REGUERO GARCIA <Tronerfull DEL REGUERO GARCIA>
2017-04-04 03:30:58 +0200

    start of the proposals table

---
+++ src/main/java/es/uniovi/asw/controllers/ProposaListController.java
@@ -0,0 +1,110 @@
+package es.uniovi.asw.controllers;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import es.uniovi.asw.infraestructure.Factories;
+import es.uniovi.asw.persistence.model.Category;
+import es.uniovi.asw.persistence.model.Citizen;
+import es.uniovi.asw.persistence.model.Proposal;
+
+@Component("ProposalListController")
+@Scope("session")
+public class ProposaListController {
+
+	private String title;
+	private String description;
+	@Autowired
+	private Factories factoria;
+
+	private Citizen citizen;
+	private List<Proposal> list;
+	private Proposal selectedProposal;
+	
+
+
+	@PostConstruct
+	public void init() {
+		list= factoria.getServicesFactory().getProposalService().findAll();
+	}
+
+
+
+	public String getTitle() {
+		return title;
+	}
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public Citizen getCitizen() {
+		return citizen;
+	}
+
+
+	public List<Proposal> getList() {
+		return list;
+	}
+
+	
+	
+
+	public Proposal getSelectedProposal() {
+		return selectedProposal;
+	}
+
+
+
+	public void setSelectedProposal(Proposal selectedProposal) {
+		this.selectedProposal = selectedProposal;
+	}
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public void setCitizen(Citizen citizen) {
+		this.citizen = citizen;
+	}
+
+
+
+	public void setList(List<Proposal> list) {
+		this.list = list;
+	}
+
+	
+	public List<Proposal> showProposals()
+	{
+		 list = factoria.getServicesFactory().getProposalService().findAll();
+		return list;
+	}
+	
+	public void selectProposal(Proposal pr)
+	{
+		selectedProposal=pr;
+		title=selectedProposal.getTitle();
+		description=selectedProposal.getDescription();
+	}
+	
+	
+
+}
