e11bc165f3dbbbffdd27fc3671756fd0ff939ca3
Diego Perez Lopez <perez@diegoprzl.com>
2017-04-06 13:02:48 +0200

    Solving Cucumber/Selenium bug

--- src/test/java/cucumberTests/RunTests.java
+++ src/test/java/cucumberTests/RunTests.java
@@ -2,8 +2,11 @@ package cucumberTests;
  
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber; 
 
 @RunWith(Cucumber.class) 
+@CucumberOptions(
+features = {"src/test/resources"})
 
 public class RunTests { }
